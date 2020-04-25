d5303cc6be620799078742a31f78a34c0eccf4d9
lowkey9904 <00657140@email.ntou.edu.tw>
2020-04-25 14:11:39 +0800

    Initial Commit

---
+++ Who can help/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Who can help
+//
+//  Created by Joker on 2020/4/25.
+//  Copyright © 2020 ntoucs. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
